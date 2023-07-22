c3c0d88a1b56287891715d90a8991c5b1ff2e8e2
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-07-22 07:06:14 +0000

    add const variables to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+/*age calculator 
+
+option to select date of birth 
+output of age in years,days,weeks
+message that its your birthday if app is done on birdate
+
+so will have a 3 items which are date, month , year which the user will input their date of birth and our online calculater will give out data in days,months, years and if the current date is the users birthday a pop up happy birthday message will apppear*/
+
+
+/* decided to use the const variable for date , month and year */
+const d1 = document.getElementById('date').value;
+const m1 = document.getElementById('month').value;
+const y1 = document.getElementById('year').value;
+
