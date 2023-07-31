c55a4725e6ed13f04322bab5c81626810c4a271e
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-07-31 11:27:33 +0000

    add output area to the html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 option to select date of birth 
 output of age in years,days,weeks
 message that its your birthday if app is done on birdate
+error message if date input is not compatible
 
 so will have a 3 items which are date, month , year which the user will input their date of birth and our online calculater will give out data in days,months, years and if the current date is the users birthday a pop up happy birthday message will apppear*/
 
@@ -11,4 +12,7 @@ so will have a 3 items which are date, month , year which the user will input th
 const d1 = document.getElementById('date').value;
 const m1 = document.getElementById('month').value;
 const y1 = document.getElementById('year').value;
+
+
+
 
